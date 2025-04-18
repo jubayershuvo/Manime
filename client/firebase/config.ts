6922d36fa32baf618f6874e16224d252bccf8bbd
Md Jubayer <mdjubayerislamshuvo34@gmail.com>
2025-04-18 08:33:31 +0600

    api keys

--- client/firebase/config.ts
+++ client/firebase/config.ts
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBJJpwz8T0z_0CLz3I2601Z7aZjl60huWY",
-  authDomain: "manime-62f47.firebaseapp.com",
-  projectId: "manime-62f47",
-  storageBucket: "manime-62f47.firebasestorage.app",
-  messagingSenderId: "504476994051",
-  appId: "1:504476994051:web:44e4031855b1dbe0e45c36",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
